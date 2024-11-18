5f9b60eb5ba8eecdc1c1da18d4764a2451639879
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-18 13:10:32 +0330

    show data in checkoutPage

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,4 +1,6 @@
 // import { useCart } from "../context/CartProvider";
+import { useSelector } from "react-redux";
+
 import BasketCard from "./../components/BasketCard";
 import BasketSidebar from "../components/BasketSidebar";
 
@@ -7,28 +9,31 @@ import styles from "./CheckoutPage.module.css";
 function CheckoutPage() {
   // const [state, dispatch] = useCart();
 
+  const state = useSelector((store) => store.cart);
+
   // const clickHandler = (type, payload) => dispatch({ type, payload });
 
-  // if (!state.itemsCounter) {
-  //   return (
-  //     <div className={styles.conatiner}>
-  //       <p>Empty</p>
-  //     </div>
-  //   );
-  // }
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.conatiner}>
+        <p>Empty</p>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.conatiner}>
-      {/* <BasketSidebar state={state} clickHandler={clickHandler} />
+      {/* <BasketSidebar state={state} clickHandler={clickHandler} /> */}
+      <BasketSidebar state={state} />
       <div className={styles.products}>
         {state.selectedItems.map((product) => (
           <BasketCard
             key={product.id}
             data={product}
-            clickHandler={clickHandler}
+            // clickHandler={clickHandler}
           />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
