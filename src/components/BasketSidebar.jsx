578794632d3b5f82e83ee1bd3f7bfd2a82d9a518
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-18 13:30:49 +0330

    create delete and add in BasketCart & handle BasketSidebar

--- src/components/BasketSidebar.jsx
+++ src/components/BasketSidebar.jsx
@@ -1,10 +1,16 @@
+import { useDispatch } from "react-redux";
+
+import { checkout } from "../features/cart/cartSlice";
+
 import { TbChecklist } from "react-icons/tb";
 import { FaHashtag } from "react-icons/fa";
 import { BsPatchCheck } from "react-icons/bs";
 
 import styles from "./BasketSidebar.module.css";
 
-function BasketSidebar({ state, clickHandler }) {
+// function BasketSidebar({ state, clickHandler }) {
+function BasketSidebar({ state }) {
+  const dispatch = useDispatch();
   return (
     <div className={styles.sidebar}>
       <div>
@@ -22,7 +28,8 @@ function BasketSidebar({ state, clickHandler }) {
         <p>Status:</p>
         <span>{!state.checkout && "Pending..."}</span>
       </div>
-      <button onClick={() => clickHandler("CHECKOUT")}>Checkout</button>
+      {/* <button onClick={() => clickHandler("CHECKOUT")}>Checkout</button> */}
+      <button onClick={() => dispatch(checkout())}>Checkout</button>
     </div>
   );
 }
