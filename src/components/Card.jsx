48c5c60e0e2ede43185a9045fb1563c837d8b41a
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-18 13:02:52 +0330

    added actions in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 import { TbListDetails, TbShoppingBagCheck } from "react-icons/tb";
 
 // import { useCart } from "../context/CartProvider";
+import {
+  addItem,
+  decrease,
+  increase,
+  removeItem,
+} from "../features/cart/cartSlice";
+
 import { shortenText, productQuantity } from "../helper/helper";
 
 import styles from "./Card.module.css";
@@ -9,15 +18,17 @@ import { MdDeleteOutline } from "react-icons/md";
 
 function Card({ data }) {
   const { id, title, image, price } = data;
+  const state = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  console.log(state);
 
   // const [state, dispatch] = useCart();
 
-  // const quantity = productQuantity(state, id);
-  const quantity = 0;
+  const quantity = productQuantity(state, id);
 
-  const clickHandler = (type) => {
-    // dispatch({ type, payload: data });
-  };
+  // const clickHandler = (type) => {
+  //   dispatch({ type, payload: data });
+  // };
 
   return (
     <div className={styles.card}>
@@ -30,23 +41,27 @@ function Card({ data }) {
         </Link>
         <div>
           {quantity === 1 && (
-            <button onClick={() => clickHandler("REMOVE_ITEM")}>
+            // <button onClick={() => clickHandler("REMOVE_ITEM")}>
+            <button onClick={() => dispatch(removeItem(data))}>
               <MdDeleteOutline />
             </button>
           )}
 
           {quantity > 1 && (
-            <button onClick={() => clickHandler("DECREASE")}>-</button>
+            // <button onClick={() => clickHandler("DECREASE")}>-</button>
+            <button onClick={() => dispatch(decrease(data))}>-</button>
           )}
 
           {!!quantity && <span>{quantity}</span>}
 
           {quantity === 0 ? (
-            <button onClick={() => clickHandler("ADD_ITEM")}>
+            // <button onClick={() => clickHandler("ADD_ITEM")}>
+            <button onClick={() => dispatch(addItem(data))}>
               <TbShoppingBagCheck />
             </button>
           ) : (
-            <button onClick={() => clickHandler("INCREASE")}>+</button>
+            // <button onClick={() => clickHandler("INCREASE")}>+</button>
+            <button onClick={() => dispatch(increase(data))}>+</button>
           )}
         </div>
       </div>
